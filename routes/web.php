c5f8a47e802ee66dc31ae66a8c7351d9c9f93771
DuongThiYenNhiB1909961 <nhib1808@gmail.com>
2023-02-15 15:58:25 +0700

    Xay dung trang introduce

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/index', function () {
     return view('index');
+});
+Route::get('/introduce', function () {
+    return view('introduce');
 });
